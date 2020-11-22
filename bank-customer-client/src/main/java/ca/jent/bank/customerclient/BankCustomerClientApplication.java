c3b1fe95ef49674943363d3a5ea906a6d012bf3a
Jocelyn Raymond <it.jraymond@gmail.com>
2020-11-22 10:30:01 -0700

    Initial setup

---
+++ bank-customer-client/src/main/java/ca/jent/bank/customerclient/BankCustomerClientApplication.java
@@ -0,0 +1,13 @@
+package ca.jent.bank.customerclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankCustomerClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankCustomerClientApplication.class, args);
+	}
+
+}
---
+++ bank-customer-client/src/test/java/ca/jent/bank/customerclient/BankCustomerClientApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.jent.bank.customerclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankCustomerClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
